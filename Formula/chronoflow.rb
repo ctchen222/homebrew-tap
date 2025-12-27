020dc12a8bb0d42a31fd2e9038c7830c0be46638
CTCHEN <abfa762466@gmail.com>
2025-12-27 13:07:20 +0800

    chore: bump chronoflow to v0.2.0

--- Formula/chronoflow.rb
+++ Formula/chronoflow.rb
@@ -1,21 +1,21 @@
 class Chronoflow < Formula
   desc "Terminal-based calendar and todo manager"
   homepage "https://github.com/ctchen222/chronoflow"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ctchen222/chronoflow/releases/download/v0.1.0/chronoflow-0.1.0-darwin-arm64.tar.gz"
-      sha256 "466e36d19b91ede236bdfc6b22f7b7712dc18de3cce2fe2a635092eb13f0ece5"
+      url "https://github.com/ctchen222/chronoflow/releases/download/v0.2.0/chronoflow-0.2.0-darwin-arm64.tar.gz"
+      sha256 "5f4dd0ec6eaa39379afbf64a3d688bfa43a59c02365d9ce420dddf44be1bf657"
 
       def install
         bin.install "chronoflow-darwin-arm64" => "chronoflow"
       end
     end
     on_intel do
-      url "https://github.com/ctchen222/chronoflow/releases/download/v0.1.0/chronoflow-0.1.0-darwin-amd64.tar.gz"
-      sha256 "8a5f3f2911d3d0c21c47167379ef5ac4e857c2fb4b6a215ad862fd38b2d7ee72"
+      url "https://github.com/ctchen222/chronoflow/releases/download/v0.2.0/chronoflow-0.2.0-darwin-amd64.tar.gz"
+      sha256 "eae0d696d4e6d369814dc563200f0ff06deddcd1cc0f9db10b93c1938c0254b0"
 
       def install
         bin.install "chronoflow-darwin-amd64" => "chronoflow"
@@ -25,16 +25,16 @@ class Chronoflow < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/ctchen222/chronoflow/releases/download/v0.1.0/chronoflow-0.1.0-linux-arm64.tar.gz"
-      sha256 "690a76c878b694fc7b83ae306707b737482dd8a1ea9c01d2ae1310b888b71077"
+      url "https://github.com/ctchen222/chronoflow/releases/download/v0.2.0/chronoflow-0.2.0-linux-arm64.tar.gz"
+      sha256 "56f30924026b8fe28bf51910efa077b297d8950fbaf5ee1a2c21948e928e3d12"
 
       def install
         bin.install "chronoflow-linux-arm64" => "chronoflow"
       end
     end
     on_intel do
-      url "https://github.com/ctchen222/chronoflow/releases/download/v0.1.0/chronoflow-0.1.0-linux-amd64.tar.gz"
-      sha256 "7971a3d1533d38621bdc295ba5f9fa7dd23fe470af3adb2fcffccac15ce758a4"
+      url "https://github.com/ctchen222/chronoflow/releases/download/v0.2.0/chronoflow-0.2.0-linux-amd64.tar.gz"
+      sha256 "6d47048610f4e97cdfee934b329e1b377007e1e944c9acf06c4be4b0662d3ad0"
 
       def install
         bin.install "chronoflow-linux-amd64" => "chronoflow"
